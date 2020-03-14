beff69968a5dab79affae050fbe37c278024ad10
Puican Cristian-Alin <puican.ca@gmail.com>
2020-03-14 16:57:15 +0200

    Class Question having fields content and author and one constructor implemented

---
+++ src/ro/ase/csie/cts/GITassignment/PuicanCristianAlin/g1092/Question.java
@@ -0,0 +1,13 @@
+package ro.ase.csie.cts.GITassignment.PuicanCristianAlin.g1092;
+
+public class Question {
+	String author;
+	String content;
+	
+	public Question(String author, String content) {
+		super();
+		this.author = author;
+		this.content = content;
+	}
+
+}
